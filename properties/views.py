9e2c85191a8bd2d5ec9c055bcd52c5b54476bdae
Loutimi <loutimi59@gmail.com>
2025-08-30 19:35:47 +0100

    Create property_list view

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from django.views.decorators.http import require_GET
 from .models import Property
 
-@cache_page(60 * 15)  # Cache for 15 minutes
+@require_GET
+@cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values("id", "title", "price", "location")
-    return JsonResponse(list(properties), safe=False)
+    properties = list(Property.objects.values())
+    return JsonResponse({
+        "data": properties
+    })
